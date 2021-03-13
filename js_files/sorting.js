ef742bb326ea93eb4ba79fc774bab5a46aacce3c
settingsingh <rahulsingh3118.rs@gmail.com>
2021-03-14 01:57:47 +0530

    tried but could not display the bars, will do tomorrow

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,16 +1,33 @@
 let divId = document.getElementById('bars');
 let arr = [];
-for (var i = 0; i < 100; i++) {
-  arr.push(Math.floor(Math.random() * 100));
+let marginSize;
+let divs = [];
+let divBar;
+const arrSize = 100;
+
+for (var i = 0; i < arrSize; i++) {
+  arr[i] = Math.floor(Math.random() * 100) + 1;
 }
+console.log(arr);
 
 const newArrayBtn = document.getElementById('new-array');
 newArrayBtn.addEventListener('click', barFunction);
 
 function barFunction() {
-  for (let i = 0; i < 100; i++) {
-    let barDiv = document.createElement('div');
-    barDiv.classList.add('bar');
-    divId.appendChild(barDiv);
+  divId.innerHTML = '';
+  for (let i = 0; i < arrSize; i++) {
+    divBar = document.createElement('div');
+    marginSize = 0.1;
+    divBar.style =
+      ' margin:0% ' +
+      marginSize +
+      '%; background-color:#fff; width:' +
+      (100 / arrSize - 2 * marginSize) +
+      '%; height:' +
+      arr[i] +
+      '%;';
+    divId.appendChild(divBar);
+    console.log(divId);
   }
 }
+console.log('divBar', divBar);
